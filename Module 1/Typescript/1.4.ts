e378b627d30195be17d2f7ae7374db617a1ff93c
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-10-30 21:25:03 +0600

    1.4 done

---
+++ Module 1/Typescript/1.4.ts
@@ -0,0 +1,30 @@
+//basic data types
+//1. string
+let firstName:string = 'Ala' //explasive data type
+// let firstName  = 'Ala' //implasive data type
+//number
+let roll:number = 123
+//boolean
+let isAdmin:boolean = false
+
+//undefined
+let x:undefined = undefined
+
+//null
+let y:null = null
+
+let d:number;
+d = 123
+// d = 'ddd'
+// d= false
+
+let friends:string[] = ['nadim','mynul']
+let eligiblaRollist:number[] = [10,20,30,40,50,60]
+
+eligiblaRollist.push(40)
+
+//tuple --> array --> order maintained--> type of values
+
+let coordinates:[number,number] = [1,5]
+
+let ageName: [number,string] =[20,'Mr.x'];
