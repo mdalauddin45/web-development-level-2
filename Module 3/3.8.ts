975da19561400800a820c88b16398b3c7a08d617
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-11-03 23:37:42 +0600

    3.8 done

---
+++ Module 3/3.8.ts
@@ -0,0 +1,32 @@
+{
+  //OOP - Polimorphism
+  class Person {
+    getSleep() {
+      console.log(`I am sleeping for 8 hours per day`);
+    }
+  }
+
+  class Student extends Person {
+    getSleep() {
+      console.log(`I am sleeping for 7 hours per day`);
+    }
+  }
+  class Developer extends Person {
+    getSleep() {
+      console.log(`I am sleeping for 6 hours per day`);
+    }
+  }
+  const getSleepingHours = (param: Person) => {
+    param.getSleep();
+  };
+
+  const person1 = new Person();
+  const person2 = new Student();
+  const person3 = new Developer();
+
+//   getSleepingHours(person1);
+//   getSleepingHours(person2);
+//   getSleepingHours(person3);
+
+
+}
