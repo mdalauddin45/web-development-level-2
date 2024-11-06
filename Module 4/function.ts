750532716983adea0cf009153c0ef3b3ac548036
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-11-06 22:22:07 +0600

    function done

--- Module 4/function.ts
+++ Module 4/function.ts
@@ -1,7 +1,17 @@
-//normal function and arrow function
-function multiply (a: number, b: number):number{
-    const result:number = a * b;
-    return result
+{
+  //normal function 
+  function multiply(a: number, b: number): number {
+    const result: number = a * b;
+    return result;
+  }
+  const result: number = multiply(3, 5);
+  console.log(result);
+
+  //arrow function
+  const sumOfTwo=(a:number,b:number):number=>{
+    return a+b;
+  }
+  const ans=sumOfTwo(3, 5);
+  console.log(ans);
+
 }
-const result: number = multiply(3,5);
-console.log(result);
