d605e6ed363e12eecaed1bef927198158e12d283
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-11-06 22:17:47 +0600

    funtion doenn

---
+++ Module 4/function.ts
@@ -0,0 +1,7 @@
+//normal function and arrow function
+function multiply (a: number, b: number):number{
+    const result:number = a * b;
+    return result
+}
+const result: number = multiply(3,5);
+console.log(result);
