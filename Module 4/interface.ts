fd8939a8a5693a76550ab56c09d8e1d9dac2cac9
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-11-06 23:21:25 +0600

    interface done

---
+++ Module 4/interface.ts
@@ -0,0 +1,21 @@
+{
+  //
+  interface Person {
+    name: string;
+    age: number | string;
+    isActive: boolean;
+    designation: string;
+  }
+  interface Person {
+    company: string;
+  }
+
+  const person1: Person = {
+    name: "Alauddin",
+    age: 25,
+    isActive: true,
+    designation: "Next level developer",
+    company: "BGCTUB",
+  };
+  //
+}
--- Module 4/type-assertion.ts
+++ Module 4/type-assertion.ts
@@ -4,10 +4,17 @@
 //   const messageLen:number = (message as string).length;
 //   console.log(messageLen);
 
+// const messageLen:number = (message.match(/TypeScript/g)||[]).length;
+function countWordOccurrences(message:string, word:string):number{
+    const messageLen:number =  message.split(word).length-1;
+    return messageLen;
+}
     const message:string="TypeScript is great. I love TypeScript!";
-    // const messageLen:number = (message.match(/TypeScript/g)||[]).length;
-    const messageLen:number =  message.split('TypeScript').length-1;
-    console.log(messageLen);
+    const word:string="TypeScript";
+    // const messageLen:number =  message.split(word).length-1;
+    // console.log(messageLen);
+    const result:number = countWordOccurrences(message,word);
+    console.log(result);
 
     
 
