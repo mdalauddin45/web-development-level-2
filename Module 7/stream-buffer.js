c25e885555ee79f683c28171bbc40e5c7debf8e3
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-11-16 13:35:10 +0600

    done

--- Module 7/stream-buffer.js
+++ Module 7/stream-buffer.js
@@ -9,11 +9,19 @@ server.on("request", (req, res) => {
   if (req.url == "/read-file" && req.method == "GET") {
     const readableStream = fs.createReadStream("./texts/read.txt");
     readableStream.on("data", (buffer) => {
+      res.statusCode=200;
       res.write(buffer);
     });
     readableStream.on("end", () => {
+      res.statusCode=200;
       res.end("Hello from world!");
     });
+    readableStream.on("error", (err) => {
+      console.log(err);
+      res.statusCode=500;
+      res.end("Somethis went wrong!");
+    });
+
   }
 });
 
