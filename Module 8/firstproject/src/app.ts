2109c35780d141889d61e110519f3fef932fba1a
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-11-18 22:53:36 +0600

    done

--- Module 8/firstproject/src/app.ts
+++ Module 8/firstproject/src/app.ts
@@ -1,11 +1,17 @@
 import express,{Request,Response} from 'express';
+import cors from 'cors';
+
 const app = express()
 const port = 3000
 
+//parsers
+app.use(express.json())
+app.use(cors())
+
 app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
+  var a=10;
+  res.send(a)
 })
 
-console.log(process.cwd());
 
 export default app
--- Module 8/firstproject/src/server.ts
+++ Module 8/firstproject/src/server.ts
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose');
-
+import app from "./app";
+import config from "./app/config";
+import mongoose from "mongoose";
+// const mongoose = require('mongoose');
 
 async function main() {
-    await mongoose.connect('process.env.DATABASE_URL');
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-import app from './app';
-const PORT=5000;
-
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-  })
+main();
