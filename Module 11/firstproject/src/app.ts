b3ac77e91a44c5e83211a114d5e69a2a80e6281e
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-11-26 22:11:41 +0600

    usre interface

---
+++ Module 11/firstproject/src/app.ts
@@ -0,0 +1,19 @@
+import express,{Request,Response} from 'express';
+import cors from 'cors';
+import { StudentRoutes } from './app/module/student/student.route';
+
+const app = express()
+
+//parsers
+app.use(express.json())
+app.use(cors())
+//application routes
+app.use('/api/v1/students',StudentRoutes)
+const getAController =(req: Request, res: Response)=>{
+  res.send("Hello World")
+}
+
+app.get('/',getAController )
+
+
+export default app
