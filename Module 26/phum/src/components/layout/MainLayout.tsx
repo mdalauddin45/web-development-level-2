b71ef9703697aa2e94a1c3e643227b2bd943f658
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2025-01-11 12:37:04 +0600

    module 26 done

--- Module 26/phum/src/components/layout/MainLayout.tsx
+++ Module 26/phum/src/components/layout/MainLayout.tsx
@@ -1,61 +1,64 @@
-import { Layout, Menu } from "antd";
-import React from "react";
+import { Layout, Menu, MenuProps } from 'antd';
 import {
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
-} from "@ant-design/icons";
-import { Outlet } from "react-router-dom";
+} from '@ant-design/icons';
+import { createElement } from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import { adminSidebarItems } from '../../routes/admin.routes';
 const { Header, Content, Footer, Sider } = Layout;
-const items = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  UserOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: React.createElement(icon),
-  label: `nav ${index + 1}`,
-}));
-export const MainLayout = () => {
+
+const MainLayout = () => {
   return (
-    <>
-      <Layout style={{height:'100vh'}}>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={(broken) => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
+    <Layout style={{ height: '100vh' }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+
+            height: '4rem',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
         >
-          <div className="demo-logo-vertical" />
-          <Menu
-            theme="dark"
-            mode="inline"
-            defaultSelectedKeys={["4"]}
-            items={items}
-          />
-        </Sider>
-        <Layout>
-          <Header style={{ padding: 0 }} />
-          <Content style={{ margin: "24px 16px 0" }}>
-            <div
-              style={{
-                padding: 24,
-                minHeight: 360,
-              }}
-            >
-             <Outlet/>
-            </div>
-          </Content>
-          <Footer style={{ textAlign: "center" }}>
-            Ant Design ©{new Date().getFullYear()} Created by Ant UED
-          </Footer>
-        </Layout>
+          <h1>PH Uni</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['4']}
+          items={adminSidebarItems}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            <Outlet />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
       </Layout>
-    </>
+    </Layout>
   );
 };
+
+export default MainLayout;
--- Module 26/phum/src/routes/routes.tsx
+++ Module 26/phum/src/routes/routes.tsx
@@ -1,32 +1,37 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
-import About from "../pages/about";
-import Contact from "../pages/Contact";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import { adminRoutes } from './admin.routes';
 
 const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<App/>,
-        children:[
-            {
-                path:'about',
-                element:<About/>
-            },
-            {
-                path:'contact',
-                element:<Contact/>
-            },
-        ]
-    },
-    {
-        path:'/login',
-        element:<Login/>
-    },
-    {
-        path:'/register',
-        element:<Register/>
-    },
-])
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/admin',
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: '/faculty',
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: '/student',
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+]);
+
 export default router;
