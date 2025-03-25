bcd5241d4c07878179343960eabcf56dc7b14856
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2025-03-25 22:59:56 +0600

    hooks store done

--- Module 24/project/src/App.tsx
+++ Module 24/project/src/App.tsx
@@ -1,12 +1,23 @@
 import "./App.css";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelectior } from "./redux/hook";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const { count } = useAppSelectior((state) => state.counter);
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
   return (
     <div>
       <h1>Counter with Redux</h1>
-      <button>increment</button>
-      <div>0</div>
-      <button>Decrement</button>
+      <button onClick={handleIncrement}>increment</button>
+      <div>{count}</div>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 }
